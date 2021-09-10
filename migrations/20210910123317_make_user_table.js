859cb777198bbee3294cde68af7308399b25f6c3
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-10 20:56:41 +0700

    add increments for users table

--- migrations/20210910123317_make_user_table.js
+++ migrations/20210910123317_make_user_table.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('users', function (table) {
+        table.increments();
         table.string('username').notNullable();
         table.string('password', [60]).notNullable();
         table.string('email').notNullable();
