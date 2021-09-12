ad945f59752fae2201287c1136d9a4098620a1be
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-12 20:50:19 +0700

    create auth user login and register

---
+++ pages/auth/register.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+
+export default function Register () {
+    const [fields, setFields] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    async function registerHandler (e) {
+        e.preventDefault();
+
+        const registerReq = await fetch('/api/user/register', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(fields)
+        })
+
+        const registerRes = await registerReq.json();
+        console.log(registerRes);
+    }
+
+    function fieldHandler (e) {
+        const getName = e.target.getAttribute('name');
+        
+        setFields({
+            ...fields,
+            [getName]: e.target.value
+        });
+        
+    } 
+
+    return(
+        <div className="bg-black text-white min-h-screen">
+            <h1 className="mb-9">Register</h1>
+
+            <form className="block" onSubmit={registerHandler}>
+                <label>Username</label>
+                <input name="username" type="text" className="block text-black" onChange={fieldHandler}/>
+                <label>Email</label>
+                <input name="email" type="email" className="block text-black" onChange={fieldHandler}/>
+                <label>Password</label>
+                <input name="password" type="password" className="block text-black" onChange={fieldHandler}/>
+                <button className="bg-yellow-900 rounded mt-2">Create User</button>
+            </form>
+        </div>
+    )
+}
