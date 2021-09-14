d5dfdc2f86c4cc1ca14cb8ad07faff2cab1a8427
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-14 20:48:38 +0700

    add authorization page with token

--- middlewares/user/authPage.js
+++ middlewares/user/authPage.js
@@ -11,3 +11,17 @@ export function unAuthPage (context) {
         return resolve('unauthorized');
     })
 }
+
+export function authPage (context) {
+    return new Promise (resolve => {
+        const { userCookie } = cookies(context);
+        if (!userCookie)
+            return context.res.writeHead(302, {
+                Location: '/auth/login/'
+            }).end();
+
+        return resolve({
+            token: userCookie
+        });
+    })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
-import Head from 'next/head'
+import { authPage } from "../middlewares/user/authPage";
 
-export default function Home() {
+export async function getServerSideProps (context) {
+  const { token } = await authPage(context);
+
+  return {
+    props: {
+      token      
+    }
+  }
+}
+
+export default function Home(props) {
+  console.log(props.token);
+
   return (
     <div className="bg-black min-h-screen text-white">
       <h1 className="text-lg">Index</h1>
