9cbf9c825376deb19f42b3e27dc20788902f1b04
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-15 11:23:55 +0700

    get book detail from book api

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,44 @@
 import { authPage } from "../middlewares/user/authPage";
+import React, {useState} from 'react';
 
 export async function getServerSideProps (context) {
   const { token } = await authPage(context);
 
+  const bookReq = await fetch('http://localhost:3000/api/book', {
+    headers: {
+      "Authorization": "Bearer " + token
+    }
+  });
+
+  const { data } = await bookReq.json();
+
   return {
     props: {
-      token      
+      data    
     }
   }
 }
 
 export default function Home(props) {
-  console.log(props.token);
+  const { data } = props;
+
+  const [books, setBooks] = useState(data)
 
   return (
     <div className="bg-black min-h-screen text-white">
-      <h1 className="text-lg">Index</h1>
-      
+      <h1 className="text-xl mb-7">Index</h1>
+
+      {books.map((book) => 
+        <div className="text-white">
+          <p>{book.tittle}</p>
+          <p>{book.author}</p>
+          <p>{book.publisher}</p>
+          <p>{book.description}</p>
+          <hr className="bg-white text-white mb-3"/>
+        </div>
+      )}
+
+
     </div>
   )
 }
