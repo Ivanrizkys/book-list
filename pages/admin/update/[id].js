74af2a9ba4bf66e51f3e48abad9cb06cd96f911f
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-19 11:24:24 +0700

    add update page

---
+++ pages/admin/update/[id].js
@@ -0,0 +1,81 @@
+import { authPage } from "../../../middlewares/admin/authPage";
+import { useState } from "react";
+
+export async function getServerSideProps (context) {
+    const { token } = await authPage(context) 
+    const { id } = context.query;
+
+    const getDetailBook = await fetch('http://localhost:3000/api/book/detail/' + id, {
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    });
+    const { data } = await getDetailBook.json()
+
+    return {
+        props: {
+            token,
+            data,
+            id
+        }
+    }
+}
+
+export default function Update (props) {
+    const { token, data, id} = props;
+    const {tittle, author, publisher, description, image} = data;
+
+    const [field, setField] = useState({
+        tittle,
+        author,
+        publisher,
+        description,
+        image
+        
+    });
+
+    async function updateHandler (e) {
+        e.preventDefault();
+        
+        const updateReq = await fetch('/api/book/update/' + id, {
+            method: "PUT",
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(field)
+        });
+
+        const updateRes = await updateReq.json();
+
+        console.log(updateRes);
+    }
+
+    function fieldHandler (e) {
+        const getName = e.target.getAttribute('name');
+
+        setField({
+            ...field,
+            [getName]: e.target.value
+        });
+    }
+    return (
+        <div className="container mx-auto bg-black min-h-screen text-white">
+            <h1 className="text-2xl mb-10">Update Book</h1>
+
+            <form onSubmit={updateHandler}>
+                <label>Tittle</label>
+                <input name="tittle" type="text" defaultValue={tittle} className="block text-black" onChange={fieldHandler}/>
+                <label>Author</label>
+                <input name="author" type="text" defaultValue={author} className="block text-black" onChange={fieldHandler}/>
+                <label>Publisher</label>
+                <input name="publisher" type="text" defaultValue={publisher} className="block text-black" onChange={fieldHandler}/>
+                <label>Description</label>
+                <textarea name="description" defaultValue={description} id="" cols="30" rows="10" className="block text-black" onChange={fieldHandler}></textarea>
+                <label>Image</label>
+                <input name="image" type="file" defaultValue={image} className="block" onChange={fieldHandler}/>
+                <button className="bg-yellow-900 rounded mt-3 py-1 px-8">Edit</button>
+            </form>
+        </div>
+    )
+}
