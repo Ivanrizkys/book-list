7f0efdcce40a518dd509547dbb726271a6c7e99c
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-30 17:07:22 +0700

    add create book

---
+++ pages/admin/create.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { authPage } from "../../middlewares/admin/authPage";
+import Router from 'next/router';
+
+export async function getServerSideProps (context) {
+    const { token } = await authPage (context);
+
+    return {
+        props: {
+            token
+        }
+    }
+}
+
+export default function Create (props) {
+    const { token } = props;
+    
+    const [field, setField] = useState({
+        tittle: "",
+        author: "",
+        publisher: "",
+        description: "",
+        image: ""
+    });
+
+    const [status, setStatus] = useState('normal')
+
+    async function createHandler (e) {
+        e.preventDefault();
+
+        const formData = new FormData()
+        Object.entries(field).forEach(([ key, values ]) => {
+            formData.append(key, values)
+        });
+
+        const createReq = await fetch('/api/book/create', {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            body: formData
+        });
+
+        if (!createReq.ok) return setStatus(`Error`)
+        const createRes = await createReq.json();
+        Router.push('/admin');
+    }
+
+    function fieldHandler (e) {
+        const getName = e.target.getAttribute('name')
+
+        setField({
+            ...field,
+            [getName]: e.target.value
+        })
+    }
+    
+    function imageHandler (e) {
+        const getName = e.target.getAttribute('name')
+
+        setField({
+            ...field,
+            [getName]: e.target.files[0]
+        })
+
+    }
+
+    return (
+        <div className="container mx-auto bg-black min-h-screen text-white">
+            <h1 className="text-2xl mb-10">Create Book</h1>
+
+            <form encType="multipart/from-data" onSubmit={createHandler}>
+                <label>Tittle</label>
+                <input name="tittle" type="text" className="block text-black" onChange={fieldHandler}/>
+                <label>Author</label>
+                <input name="author" type="text" className="block text-black" onChange={fieldHandler}/>
+                <label>Publisher</label>
+                <input name="publisher" type="text" className="block text-black" onChange={fieldHandler}/>
+                <label>Description</label>
+                <textarea name="description" id="" cols="30" rows="10" className="block text-black" onChange={fieldHandler}></textarea>
+                <label>Image</label>
+                <input name="image" type="file" className="block" onChange={imageHandler}/>
+                <button className="bg-yellow-900 rounded mt-3 py-1 px-8">Edit</button>
+                <p>Status {status}</p>
+            </form>
+        </div>
+    )
+}
--- pages/api/book/create.js
+++ pages/api/book/create.js
@@ -1,22 +1,78 @@
 import db from '../../../libs/db';
 import authApiForAdmin from '../../../middlewares/admin/authApiForAdmin';
+import formidable from 'formidable';
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
 
 export default async function handler (req, res) {
     if(req.method !== "POST") return res.status(401).end();
-
+    
     const auth = await authApiForAdmin(req, res);
 
-    const {tittle, author, publisher, description} = req.body;
+    const promise = new Promise ((resolve, reject) => {
+        const form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+        form.uploadDir = "./public/cover";
 
-    if(!tittle || !author || !publisher) return res.status(401).end();
-
-    const create = await db('book').insert({tittle, author, publisher, description});
-
-    const createdBook = await db('book').where({id: create}).first();
-
-    res.status(200);
-    res.json({
-        message:"create book succesfully",
-        data: createdBook
+        form.parse (req, (err, fields, files) => {
+            if (err) reject(err);
+            resolve({fields,files})
+        });
     });
+
+    return promise.then( async ({fields, files}) => {
+        const { tittle, author, publisher, description, image} = fields;
+        
+        if(!tittle || !author || !publisher) return res.status(401).end();
+
+        if (!files.image) return res.status(401).end();
+
+        // console.log(files);
+        const { image:{ path } } = files;
+        // console.log(typeof path)
+        // path.replace("public\\", "");
+        // console.log(path);
+        const create = await db('book').insert({tittle, author, publisher, description, image: path});
+        console.log(create);
+        const createdBook = await db('book').where({id: create}).first();
+        // console.log( createdBook );
+
+
+        res.status(200);
+        res.json({
+            message: "create data succesfully",
+            data: createdBook
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+    // if(req.method !== "POST") return res.status(401).end();
+
+    // const auth = await authApiForAdmin(req, res);
+
+    // const {tittle, author, publisher, description} = req.body;
+
+    // if(!tittle || !author || !publisher) return res.status(401).end();
+
+    // const create = await db('book').insert({tittle, author, publisher, description});
+
+    // const createdBook = await db('book').where({id: create}).first();
+
+    // res.status(200);
+    // res.json({
+    //     message:"create book succesfully",
+    //     data: createdBook
+    // });
 }
