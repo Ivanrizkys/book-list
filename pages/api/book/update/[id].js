7b3f0ce8f06ee848f4b659c5b029a9010567f28a
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-09-30 17:09:53 +0700

    modify update api using formidable

--- pages/api/book/update/[id].js
+++ pages/api/book/update/[id].js
@@ -1,5 +1,14 @@
 import db from '../../../../libs/db';
 import authApiForAdmin from '../../../../middlewares/admin/authApiForAdmin';
+import formidable from 'formidable';
+
+const fs = require('fs');
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
 
 export default async function handler (req, res) {
     if(req.method !== 'PUT') return res.status(401).end();
@@ -8,15 +17,40 @@ export default async function handler (req, res) {
 
     const { id } = req.query;
 
-    const {tittle, author, publisher, description, image} = req.body;
+    const promise = new Promise((resolve, reject) => {
+        const form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+        form.uploadDir = "./public/cover"
 
-    const updateBook = await db('book').where({id}).update({tittle, author, publisher, description, image})
+        form.parse(req, (err, fields, files) => {
+            if (err) reject(err);
+            resolve({fields,files})
+        })
+    })
 
-    const resUpdateBook = await db('book').where({id}).first();
+    return promise.then( async ({fields, files}) => {
+        const {tittle, author, publisher, description} = fields;
+        if (!tittle || !author || !publisher || !description) return res.status(401).end()
+        
+        if (files.image) {
+            const { image: { path } } = files;
+            const { image } = await db('book').select('image').where({id}).first();
+            fs.unlink(image, err => {
+                if (err) console.log(err);
+            })
 
-    res.status(200);
-    res.json({
-        message: "update data succesfully",
-        data: resUpdateBook
-    });
+            const upImageData = await db('book').where({id}).update({image: path});
+        }
+
+        const updateBook = await db('book').where({id}).update({tittle, author, publisher, description});
+        const resUpdatedBook = await db('book').where({id}).first();
+
+        res.status(200);
+        res.json({
+            message: "update book succesfully",
+            data: resUpdatedBook
+        });
+        
+    })
+
 }
