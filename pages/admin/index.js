786cda9f850f500d9f00016527928900e08c2c44
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-10-03 20:02:46 +0700

    add delete data

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,18 +1,15 @@
 import { authPage } from "../../middlewares/admin/authPage";
 import Router from 'next/router';
-
+import { useState } from "react";
 
 export async function getServerSideProps (context) {
     const { token } = await authPage(context);
-
     const bookReq = await fetch('http://localhost:3000/api/book', {
         headers: {
             "Authorization": "Bearer " + token
         }
     });
-    
     const { data } = await bookReq.json();
-    
     return {
         props: {
             token,
@@ -23,9 +20,30 @@ export async function getServerSideProps (context) {
 
 export default function AdminHome (props) {
     const {token, data} = props;
+    const [book, setBook] = useState(data);
 
     function editHandler (id, e) {
+        e.preventDefault();
+
         Router.push('/admin/update/' + id);
+    }
+
+    async function deleteHandler (id, e) {
+        e.preventDefault();
+
+        const ask = confirm(`Apakah anda yakin akan menghapus buku dengan id ${id}`);
+        if (!ask) return;
+        
+        const deleteReq = await fetch('/api/book/delete/' + id, {
+            method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        });
+        if (!deleteReq.ok) return alert(`Tidak bisa menghapus buku dengan id ${id}`);
+        const { data } = await deleteReq.json();
+        setBook(data);
+
     }
 
     return (
@@ -33,16 +51,15 @@ export default function AdminHome (props) {
             <div className="container mx-auto">
                 <h1 className="text-xl text-center mb-10">Admin Page</h1>
             </div>
-
             
             <div className="container mx-auto flex-col">
-                {data.map(books => 
+                {book.map(books => 
                     <div key={books.id} className="border border-yellow-700 flex">
                         <div className="w-4/12 border-r border-yellow-700">{books.tittle}</div>
                         <div className="w-4/12 border-r border-yellow-700">{books.author}</div>
                         <div className="w-4/12 border-r border-yellow-700">
-                            <button className="w-2/12">Hapus</button>
-                            <button className="w-2/12" onClick={editHandler.bind(this, books.id)}>Edit</button>
+                            <button onClick={deleteHandler.bind(this, books.id)} className="w-2/12">Hapus</button>
+                            <button onClick={editHandler.bind(this, books.id)} className="w-2/12">Edit</button>
                         </div>
                     </div>
                 )}
