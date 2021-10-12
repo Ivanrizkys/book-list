184cefd01c2b8c534049d86f4d5fb3ed9a0b34ce
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-10-12 17:36:30 +0700

    delete image from folder

--- pages/api/book/delete/[id].js
+++ pages/api/book/delete/[id].js
@@ -1,5 +1,6 @@
 import db from '../../../../libs/db';
 import authApiForAdmin from '../../../../middlewares/admin/authApiForAdmin';
+import fs from 'fs';
 
 export default async function handler (req, res) {
     if (req.method !== "DELETE") return res.status(401).end();
@@ -7,7 +8,11 @@ export default async function handler (req, res) {
     const auth = await authApiForAdmin(req, res);
 
     const {id} = req.query;
+    const {image} = await db('book').select('image').where({id}).first();
     const deletedBook = await db('book').where({id}).del();
+    fs.unlink(image, err => {
+        if (err) res.status(401).json({message: err});
+    });
     const bookUpdated = await db('book');
     
     res.status(200);
