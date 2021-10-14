12d1cc45406db987c6b438f9a4785a1c6d5b9e11
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-10-14 19:10:28 +0700

    adding update user

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Router from 'next/router';
 import TittleAdmin from "../../../components/TittleAdmin";
 import { authPage } from "../../../middlewares/admin/authPage";
 
@@ -10,7 +11,6 @@ export async function getServerSideProps (context) {
         }
     });
     const {data} = await users.json();
-    console.log(token, data);
     return {
         props: {
             token,
@@ -23,19 +23,28 @@ export default function Users ({token, data}) {
     const [users, setUsers] = useState(data);
     const [search, setSearch] = useState("");
 
-    function deleteHandler (id, username, e) {
+    async function deleteHandler (id, username, e) {
         e.preventDefault();
+        const ask = confirm(`Apakah anda akan menghapus user ${username}`);
+        if (!ask) return;
 
+        const deleteReq = await fetch('/api/user/delete/' + id,{
+            method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + token,
+            }
+        });
+        if (!deleteReq.ok) return alert(`Tidak bisa menghapus user ${username}`);
+        const {data} = await deleteReq.json();
+        setUsers(data);
     }
 
-    function editHandler (username, e) {
+    function editHandler (id, e) {
         e.preventDefault();
-        console.log(username);
-
+        Router.push('/admin/users/update/' + id);
     }
 
     useEffect(() => {
-        // console.log(search);
         const filter = data.filter(({username}) => {
             return (
                 username.toLowerCase().includes(search.toLowerCase())
@@ -43,6 +52,7 @@ export default function Users ({token, data}) {
         });
         setUsers(filter);
     }, [search])
+
     return (
         <>
             <TittleAdmin/>
@@ -58,7 +68,7 @@ export default function Users ({token, data}) {
                             <div className="w-4/12 border-r border-yellow-700">{user.email}</div>
                             <div className="w-4/12 border-r border-yellow-700">
                                 <button onClick={deleteHandler.bind(this, user.id, user.username)} className="w-2/12">Hapus</button>
-                                <button onClick={editHandler.bind(this, user.username)} className="w-2/12">Edit</button>
+                                <button onClick={editHandler.bind(this, user.id)} className="w-2/12">Edit</button>
                             </div>
                         </div>
                     ) :
