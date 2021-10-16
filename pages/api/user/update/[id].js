acb8255d38029d8016f06c8f3a63331fcb8d33ff
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-10-16 09:28:23 +0700

    change where update query and rename file

--- pages/api/user/update/[id].js
+++ pages/api/user/update/[id].js
@@ -4,13 +4,12 @@ import authApiForAdmin from '../../../../middlewares/admin/authApiForAdmin';
 export default async function handler (req, res) {
     if (req.method !== 'PUT') return res.status(401).end();
     const auth = await authApiForAdmin(req, res);
-    const {name} = req.query;
+    const {id} = req.query;
     const {username, email} = req.body;
     if (!username || !email) return res.status(401).end();
 
-    const updateUser = await db('users').where({username:name}).update({username, email});
+    const updateUser = await db('users').where({id}).update({username, email});
     const resUpdateUser = await db('users').where({username}).first();
-    console.log(resUpdateUser);
 
     res.status(200);
     res.json({
